3ef10922ef17c616b126fd3cfbaf6ff88cf64b94
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-02-22 23:23:00 +0100

    classes

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,9 @@
+import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
 
+const ironman = new Hero('Ironman', 1, 55);
+// const ironman = new HeroClasses.Hero('Ironman', 1, 55);
 
-console.log('Hola Mundo!');
-
-
-
-
-
-
-
+console.log(ironman);
+console.log(ironman.power);
