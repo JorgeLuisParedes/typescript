121fd26d1bb99ac53ed93cf3c3ed57616b3c9bc1
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-02-23 12:08:33 +0100

    decoradores

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,5 +1,9 @@
-import getPokemon from "./generics/get-pokemon";
-getPokemon(4)
-    .then(pokemon => console.log(pokemon.sprites.front_default))
-    .catch(error => console.log(error))
-    .finally(() => { console.log('Fin de getPokemon') })
+import { Pokemon } from "./decorators/pokemon-class";
+
+const charmander = new Pokemon('Charmander');
+// console.log(charmander);
+// (Pokemon.prototype as any).customName = 'Pikachu';
+// charmander.savePokemonToDB(10);
+
+charmander.publickApi = 'https://fernando-herrera.com';
+console.log(charmander);
